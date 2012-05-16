9d7edee0b27bdf6267a906beec26e095128d31da
DK <dankim007@me.com>
2012-05-15 21:55:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spitfire::Application.config.secret_token = '9a9e89ac7e6f11921d727b0f6b6c7e72826520a9961e2283e3280d05822197c3a6a89eaf6a438dbca1aca7ca68a5af08dd76241e93fc072730dbb2999d0cdd32'
